c33bc29281d161d96dc4b47a65eded9cf5a407cf
Javier Carrillo <jcentercreation@gmail.com>
2022-05-21 17:53:59 +0200

    Initial Commit

---
+++ BubbleAppV2/BubbleAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleAppV2App.swift
+//  BubbleAppV2
+//
+//  Created by Javier Carrillo Gallego on 21/5/22.
+//
+
+import SwiftUI
+
+@main
+struct BubbleAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubbleAppV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BubbleAppV2
+//
+//  Created by Javier Carrillo Gallego on 21/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
